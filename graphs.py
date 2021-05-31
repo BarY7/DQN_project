df2e13d709134f94f97449074c1bcadcd949e815
BarY7 <baryaacovi7@gmail.com>
2021-05-31 22:28:03 +0300

    updated graphs

--- graphs.py
+++ graphs.py
@@ -1,8 +1,11 @@
 import numpy as np 
 import pickle
 import matplotlib.pyplot as plt
+import pandas as pd
 
-with open('./statistics.pkl', "rb") as pickled_file:
+with open('./statistics copy.pkl', "rb") as pickled_file:
   pickled_data = pickle.load(pickled_file)
   print("OK")
+  statistics_df = pd.DataFrame(pickled_data)
+  statistics_df.plot(xlabel = 't',ylabel = 'reward', title = 'Reward vs. Time').get_figure().savefig('./4M' + '.png') 
   pickled_file.close()
